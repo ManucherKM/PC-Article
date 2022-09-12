974fcf55029bab150799c91541fa6f3a1fc53b8d
ManucherKM <91463158+ManucherKM@users.noreply.github.com>
2022-09-12 20:27:18 +0300

    Add files via upload

--- controlles/PostController.js
+++ controlles/PostController.js
@@ -1,13 +1,13 @@
 import PostModel from "../models/Post.js";
 
-export const create = async (req, res) => {
+export const createPost = async (req, res) => {
     try {
         const body = req.body
 
         const doc = new PostModel({
             title: body.title,
             text: body.text,
-            author: body.userId,
+            author: req.userId,
             imageUrl: body.imageUrl,
             viewsCount: body.viewsCount,
             tags: body.tags
@@ -22,6 +22,54 @@ export const create = async (req, res) => {
 
         res.status(500).json({
             message: "Не удалось создать пост"
+        })
+    };
+};
+
+export const updatePost = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: "Не удалось обновить пост"
+        })
+    };
+};
+
+export const deletePost = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: "Не удалось удалить пост"
+        })
+    };
+};
+
+export const getPost = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: "Не удалось обновить пост"
+        })
+    };
+};
+
+export const getOnePost = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log(err);
+
+        res.status(500).json({
+            message: "Не удалось обновить пост"
         })
     };
 };
